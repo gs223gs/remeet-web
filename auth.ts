9bf1fe5147a9eeec17db6f8abdd4b3dbe4759fc9
T.Miura <mtaiga0213@icloud.com>
2025-10-11 01:22:01 +0900

    feat: DB でのsession 管理からjwt に変更

--- auth.ts
+++ auth.ts
@@ -6,4 +6,5 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [GitHub],
+  session: { strategy: "jwt" },
 });
