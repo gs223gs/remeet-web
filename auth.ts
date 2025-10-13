3a81dff1880717294bec5bcc836a422829c66c15
T.Miura <mtaiga0213@icloud.com>
2025-10-13 21:23:06 +0900

    feat:  getUser関数を仮作成 user検証のため

--- auth.ts
+++ auth.ts
@@ -8,3 +8,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   session: { strategy: "jwt" },
 });
+
+//TODO 絶対間違っているから後でやる
+export const getUser = async () => {
+  const session = await auth();
+  return session?.user;
+};
