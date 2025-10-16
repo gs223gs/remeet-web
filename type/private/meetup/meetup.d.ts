d383e5760ba91859fb0be63546f4a5a459400cc2
T.Miura <mtaiga0213@icloud.com>
2025-10-16 23:25:35 +0900

    feat: meetup/[id] の型を作成

--- type/private/meetup/meetup.d.ts
+++ type/private/meetup/meetup.d.ts
@@ -1,6 +1,28 @@
+import { Result } from "@/type/error/error";
+
 export type MeetupErrors = {
   name?: string[];
   scheduledAt?: string[];
   auth?: "認証に失敗しました"; //TODO これ汎用性高くしたい
   server?: "server error";
 };
+
+export type MeetupDetail = {
+  id: string;
+  name: string;
+  scheduledAt: Date;
+};
+
+export type MeetupDetailContact = {
+  name: string;
+  company?: string;
+  role?: string;
+  tags: string[];
+};
+
+export type MeetupDetailSummary = {
+  detail: MeetupDetail;
+  contacts: MeetupDetailContact[];
+};
+
+export type MeetupDetailResult = Result<MeetupDetailSummary>;
