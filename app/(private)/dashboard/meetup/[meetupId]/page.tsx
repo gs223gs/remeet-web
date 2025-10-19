28dfa11570df5523854f523df7368049eef85325
T.Miura <mtaiga0213@icloud.com>
2025-10-19 20:15:38 +0900

    fix: promise のつけ忘れを修正

--- app/(private)/dashboard/meetup/[meetupId]/page.tsx
+++ app/(private)/dashboard/meetup/[meetupId]/page.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import { getMeetupDetailSummary } from "../_server/server";
 
 export default async function MeetupDetail({
   params,
 }: {
-  params: { meetupId: string };
+  params: Promise<{ meetupId: string }>;
 }) {
   const { meetupId } = await params;
   const detail = await getMeetupDetailSummary(meetupId);
@@ -12,23 +13,28 @@ export default async function MeetupDetail({
     <div>
       <div className=" text-3xl">出会った人:{detail.data.contactCount}</div>
       <div>meetup名: {detail.data.detailWithContacts.detail.name}</div>
-      <div className="m-1">
+      <div className="m-1 flex ">
         {detail.data.detailWithContacts.contacts.map((c) => {
           return (
-            <div key={c.id} className=" outline-2 m-2">
-              <div>{c.name}</div>
-              <div>{c.role}</div>
-              <div>{c.company}</div>
-              <div>
-                {c.tags.map((t) => {
-                  return (
-                    <div key={t} className=" outline-2 m-2">
-                      {t}
-                    </div>
-                  );
-                })}
+            <Link
+              key={c.id}
+              href={`/dashboard/meetup/${meetupId}/contacts/${c.id}/`}
+            >
+              <div className=" outline-2 m-2">
+                <div>{c.name}</div>
+                <div>{c.role}</div>
+                <div>{c.company}</div>
+                <div>
+                  {c.tags.map((t) => {
+                    return (
+                      <div key={t} className=" outline-2 m-2">
+                        {t}
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
+            </Link>
           );
         })}
       </div>
