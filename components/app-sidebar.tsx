c6a0114606cc1e9b6c1a9d19e37d01e877ede8bd
T.Miura <mtaiga0213@icloud.com>
2025-10-21 12:46:13 +0900

    feat: sidebarの実装 ドキュメントを参考に

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -5,14 +5,31 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar";
+import { Home } from "lucide-react";
+import Link from "next/link";
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader className="text-3xl">ReMeet</SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <Link href={"/dashboard/"}>
+            <span className=" flex  gap-2">
+              <Home />
+              HOME
+            </span>
+          </Link>
+        </SidebarGroup>
+        <SidebarGroup>
+          <Link href={"/dashboard/meetup"}>meetup</Link>
+        </SidebarGroup>
+        <SidebarGroup>
+          <Link href={"/dashboard/contscts"}>contacts</Link>
+        </SidebarGroup>
+        <SidebarGroup>
+          <Link href={"/dashboard/tag"}>tag</Link>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
