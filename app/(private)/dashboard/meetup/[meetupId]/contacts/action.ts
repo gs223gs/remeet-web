c4b806403c75051356de4a3300b955da6986971c
T.Miura <mtaiga0213@icloud.com>
2025-10-25 01:21:29 +0900

    feat: validation を追加

--- app/(private)/dashboard/meetup/[meetupId]/contacts/action.ts
+++ app/(private)/dashboard/meetup/[meetupId]/contacts/action.ts
@@ -3,6 +3,10 @@ import { prisma } from "@/lib/prisma";
 import { getUser } from "@/auth";
 import { ActionState } from "@/type/util/action";
 import { ContactsErrors } from "@/type/private/contacts/contacts";
+import {
+  createContactsSchema,
+  CreateContactsSchema,
+} from "@/validations/private/contactsValidation";
 
 //TODO 型作成
 export const createContacts = async (
@@ -10,9 +14,31 @@ export const createContacts = async (
   _: ActionState<ContactsErrors>,
   formData: FormData,
 ): Promise<ActionState<ContactsErrors>> => {
+  console.log(formData);
+  console.log(formData.getAll("tagId"));
   //TODO formdata 取得
   //TODO validation
+  const validatedFields = createContactsSchema.safeParse({
+    name: formData.get("name"),
+    company: formData.get("company"),
+    role: formData.get("role"),
+    description: formData.get("description"),
+    tags: formData.getAll("tags"),
+    githubHandle: formData.get("githubHandle"),
+    githubId: formData.get("githubId"),
+    twitterHandle: formData.get("twitterHandle"),
+    twitterId: formData.get("twitterId"),
+    websiteHandle: formData.get("websiteHandle"),
+    websiteUrl: formData.get("websiteUrl"),
+    productHandle: formData.get("productHandle"),
+    productUrl: formData.get("productUrl"),
+    otherHandle: formData.get("otherHandle"),
+    other: formData.get("other"),
+  });
 
+  console.log("VALIDATION DATA", validatedFields.data?.name);
+  console.log("VALIDATION SUCCESS", validatedFields.success);
+  console.log("VALIDATION ERRORS", validatedFields.error);
   try {
     const user = await getUser();
     if (!user)
@@ -26,7 +52,16 @@ export const createContacts = async (
     //TODO contactTag へのinsert
 
     //TODO prismaで同時に行けるか調べる
+
+    return {
+      success: true,
+      errors: {},
+    };
   } catch (error) {
     console.log(error);
+    return {
+      success: false,
+      errors: {},
+    };
   }
 };
