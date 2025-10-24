acc524734d3fd4e5f44738f5407369261aaeba3e
T.Miura <mtaiga0213@icloud.com>
2025-10-24 23:01:34 +0900

    feat: フォームの仮実装

---
+++ components/contacts/form/requiredForm.tsx
@@ -0,0 +1,142 @@
+"use client";
+type Props = {
+  meetupId: string;
+  tags: Tag[];
+};
+
+import { useActionState, useState } from "react";
+import { createContacts } from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/action";
+import { Tag } from "@/type/private/contacts/contacts";
+export default function RequiredForm({ meetupId, tags }: Props) {
+  //formをCSR ページをSSR にする ページでタグを取得して form に props で渡す
+  const createContactsWithMeetupId = createContacts.bind(null, meetupId);
+  const [contactTags, setContactTags] = useState<Tag[]>([...tags]);
+  const [selectTags, setSelectTags] = useState<Tag[]>([]);
+  const [tag, setTag] = useState<string>("");
+  const [state, action, isPending] = useActionState(
+    createContactsWithMeetupId,
+    {
+      success: false,
+      errors: {},
+    },
+  );
+
+  if (isPending) return <div>pending</div>;
+  return (
+    <form action={action} className="flex flex-col m-4 outline">
+      name:
+      <input type="text" name="name" placeholder="name" />
+      comp: <input type="text" name="company" placeholder="company" />
+      role: <input type="text" name="role" placeholder="role" />
+      disc: <textarea name="description" />
+      <div>
+        <input
+          type="text"
+          placeholder="タグを追加"
+          onChange={(e) => {
+            setTag(e.target.value);
+          }}
+          value={tag}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            //ここでDBにtagをsubmit して返り値を取りたい
+            // createTag() //user id は session から
+            setSelectTags([{ tagId: "", name: tag }, ...selectTags]);
+            setTag("");
+          }}
+        >
+          追加
+        </button>
+      </div>
+      <div className="outline m-5">
+        {contactTags.map((t) => {
+          return (
+            <div
+              className="outline"
+              key={t.tagId}
+              onClick={() => {
+                setSelectTags([...selectTags, t]);
+                setContactTags([]);
+                console.log("clicked");
+              }}
+            >
+              {t.name}
+            </div>
+          );
+        })}
+      </div>
+      <div className="outline m-5">
+        {/*TODO あとでセレクトボックスにする */}
+        {selectTags.map((t) => {
+          return (
+            <div key={t.tagId} className="outline flex">
+              <input defaultValue={t.tagId} name="tagId" type="hidden" />
+              <span>{t.name}</span>
+              <button
+                onClick={() => {
+                  setSelectTags([...selectTags.filter((st) => st != t)]);
+                }}
+                className=" outline"
+                type="button"
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <span>
+          handle:
+          <input type="text" name="githubHandle" />
+        </span>
+        <span>
+          id:
+          <input type="text" name="github" />
+        </span>
+      </div>
+      <div>
+        <span>
+          handle:
+          <input type="text" name="twitterHandle" />
+        </span>
+        <span>
+          id:
+          <input type="text" name="twitter" />
+        </span>
+      </div>
+      <div>
+        <span>
+          handle:
+          <input type="text" name="websiteHandle" />
+        </span>
+        <span>
+          id:
+          <input type="text" name="website" />
+        </span>
+      </div>
+      <div>
+        <span>
+          handle:
+          <input type="text" name="productHandle" />
+        </span>
+        <span>
+          id:
+          <input type="text" name="product" />
+        </span>
+      </div>
+      <div>
+        <span>
+          handle:
+          <input type="text" name="otherHandle" />
+          <span>
+            id: <input type="text" name="other" />
+          </span>
+        </span>
+      </div>
+      <button type="submit">送信</button>
+    </form>
+  );
+}
