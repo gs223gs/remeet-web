06109fbe2017a835e85402f450af8ec27c02911f
T.Miura <mtaiga0213@icloud.com>
2025-10-29 22:18:36 +0900

    feat: 全タグの表示

--- app/(private)/dashboard/tags/page.tsx
+++ app/(private)/dashboard/tags/page.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { getTags } from "./_server/server";
 
-export default function Tags() {
-  return <div>TagPage</div>;
+export default async function Tags() {
+  const tags = await getTags();
+  if (!tags.ok)
+    return (
+      <p>
+        {tags.error.message.map((e) => {
+          return <div key={e}>{e}</div>;
+        })}
+      </p>
+    );
+  return (
+    <div className="grid grid-cols-3 ">
+      {tags.data.map((t) => {
+        return (
+          <div key={t.id} className="outline m-5">
+            {t.name}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
