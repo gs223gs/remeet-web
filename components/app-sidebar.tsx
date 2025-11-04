990dafe80caabc4bfa129f177856788a237693a2
T.Miura <mtaiga0213@icloud.com>
2025-11-04 20:24:52 +0900

    feat: sidebar のレスポンシブ対応

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -24,119 +24,65 @@ import {
 // ];
 export function AppSidebar() {
   const { open, isMobile } = useSidebar();
-  if (isMobile) {
-    return (
-      <>
-        {!open && <SidebarTrigger />}
-        <Sidebar collapsible="icon">
+
+  return (
+    <>
+      {!open && isMobile && <SidebarTrigger />}
+      <Sidebar collapsible="icon">
+        {open ? (
           <SidebarHeader>
             <div className="flex">
               <div className="text-3xl ">ReMeet</div>
               <SidebarTrigger />
             </div>
           </SidebarHeader>
-          <SidebarContent>
-            <SidebarGroup>
-              <Link href={"/dashboard"}>
-                <span className=" flex  gap-2">
-                  {/**あとでコンポーネント化する */}
-                  <Home />
-                  HOME
-                </span>
-              </Link>
-            </SidebarGroup>
-            <SidebarGroup>
-              <Link href={"/dashboard/meetup"}>
-                <span className=" flex  gap-2">
-                  <Building />
-                  MEETUPS
-                </span>
-              </Link>
-            </SidebarGroup>
-            <SidebarGroup>
-              <Link href={"/dashboard/contacts"}>
-                <span className=" flex  gap-2">
-                  <Users />
-                  CONTACTS
-                </span>
-              </Link>
-            </SidebarGroup>
-            <SidebarGroup>
-              <Link href={"/dashboard/tags"}>
-                <span className=" flex  gap-2">
-                  <Tag />
-                  TAGS
-                </span>
-              </Link>
-            </SidebarGroup>
-          </SidebarContent>
-          <SidebarFooter>
-            {/* <Link href={"/dashboard/profile"}> */}
-            <span className=" flex  gap-2">
-              <CircleUserRound />
-              PROFILE
-            </span>
-            {/* </Link> */}
-          </SidebarFooter>
-        </Sidebar>
-      </>
-    );
-  }
-  return (
-    <Sidebar collapsible="icon">
-      {open ? (
-        <SidebarHeader>
-          <div className="flex">
-            <div className="text-3xl ">ReMeet</div>
-            <SidebarTrigger />
-          </div>
-        </SidebarHeader>
-      ) : (
-        <SidebarTrigger />
-      )}
-      <SidebarContent>
-        <SidebarGroup>
-          <Link href={"/dashboard"}>
-            <span className=" flex  gap-2">
-              {/**あとでコンポーネント化する */}
-              <Home />
-              {open && "HOME"}
-            </span>
-          </Link>
-        </SidebarGroup>
-        <SidebarGroup>
-          <Link href={"/dashboard/meetup"}>
-            <span className=" flex  gap-2">
-              <Building />
-              {open && "MEETUPS"}
-            </span>
-          </Link>
-        </SidebarGroup>
-        <SidebarGroup>
-          <Link href={"/dashboard/contacts"}>
-            <span className=" flex  gap-2">
-              <Users />
-              {open && "CONTACTS"}
-            </span>
-          </Link>
-        </SidebarGroup>
-        <SidebarGroup>
-          <Link href={"/dashboard/tags"}>
-            <span className=" flex  gap-2">
-              <Tag />
-              {open && "TAGS"}
-            </span>
-          </Link>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        {/* <Link href={"/dashboard/profile"}> */}
-        <span className=" flex  gap-2">
-          <CircleUserRound />
-          {open && "PROFILE"}
-        </span>
-        {/* </Link> */}
-      </SidebarFooter>
-    </Sidebar>
+        ) : (
+          <SidebarTrigger />
+        )}
+        <SidebarContent>
+          <SidebarGroup>
+            <Link href={"/dashboard"}>
+              <span className=" flex  gap-2">
+                {/**あとでコンポーネント化する */}
+                <Home />
+                {isMobile ? "HOME" : open && "HOME"}
+              </span>
+            </Link>
+          </SidebarGroup>
+          <SidebarGroup>
+            <Link href={"/dashboard/meetup"}>
+              <span className=" flex  gap-2">
+                <Building />
+                {isMobile ? "MEETUP" : open && "MEETUP"}
+              </span>
+            </Link>
+          </SidebarGroup>
+          <SidebarGroup>
+            <Link href={"/dashboard/contacts"}>
+              <span className=" flex  gap-2">
+                <Users />
+                {isMobile ? "CONTACTS" : open && "CONTACTS"}
+              </span>
+            </Link>
+          </SidebarGroup>
+          <SidebarGroup>
+            <Link href={"/dashboard/tags"}>
+              <span className=" flex  gap-2">
+                <Tag />
+                {isMobile ? "TAGS" : open && "TAGS"}
+              </span>
+            </Link>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          {/* <Link href={"/dashboard/profile"}> */}
+          <span className=" flex  gap-2">
+            <CircleUserRound />
+            {isMobile ? "PROFILE" : open && "PROFILE"}
+          </span>
+          {/* </Link> */}
+        </SidebarFooter>
+      </Sidebar>
+    </>
   );
 }
