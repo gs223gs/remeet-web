f050ede68b51a2af6cdd7ed50c448c638ad7a2bc
T.Miura <mtaiga0213@icloud.com>
2025-11-12 23:08:19 +0900

    refactor: NewTagコンポーネントの分割 Fixes #86

--- components/contacts/form/requiredForm.tsx
+++ components/contacts/form/requiredForm.tsx
@@ -4,20 +4,19 @@ type Props = {
   tags: Tag[];
 };
 
-import { useActionState, useState, useTransition } from "react";
+import { useActionState, useState } from "react";
 
 import type { Tag } from "@/type/private/tags/tags";
 
-import {
-  createContacts,
-  createTag,
-} from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/action";
+import { createContacts } from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/action";
+import { NewTag } from "@/components/tag/form/newTag";
+
 export default function RequiredForm({ meetupId, tags }: Props) {
-  //formをCSR ページをSSR にする ページでタグを取得して form に props で渡す
   const createContactsWithMeetupId = createContacts.bind(null, meetupId);
   const [contactTags, setContactTags] = useState<Tag[]>([...tags]);
   const [selectTags, setSelectTags] = useState<Tag[]>([]);
   const [newTag, setNewTag] = useState<string>("");
+
   const [state, action, isPending] = useActionState(
     createContactsWithMeetupId,
     {
@@ -27,6 +26,7 @@ export default function RequiredForm({ meetupId, tags }: Props) {
   );
 
   if (isPending) return <div>pending</div>;
+
   return (
     <form action={action} className="flex flex-col m-4 outline">
       {state.success ? <p>true</p> : <p>false</p>}
@@ -133,67 +133,3 @@ export default function RequiredForm({ meetupId, tags }: Props) {
     </form>
   );
 }
-
-type NewTagProps = {
-  setNewTag: (s: string) => void;
-  newTag: string;
-  selectTags: Tag[];
-  setSelectTags: (t: Tag[]) => void;
-};
-export const NewTag = ({
-  newTag,
-  setNewTag,
-  setSelectTags,
-  selectTags,
-}: NewTagProps) => {
-  const [isPending, startTransition] = useTransition();
-  const [functionMessage, setFunctionMessage] = useState<string[]>([]);
-  const addTag = async () => {
-    const createdTags = await createTag(newTag);
-    if (createdTags.ok) {
-      setSelectTags([
-        {
-          id: createdTags.data.id,
-          name: createdTags.data.name,
-        },
-        ...selectTags,
-      ]);
-      setNewTag("");
-      setFunctionMessage([]);
-    }
-    if (!createdTags.ok) {
-      setFunctionMessage(createdTags.error.message);
-    }
-  };
-  if (isPending) return <p>追加中</p>;
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="タグを追加"
-        onChange={(e) => {
-          setNewTag(e.target.value);
-        }}
-        onKeyDown={(e) => {
-          if (e.nativeEvent.isComposing) return;
-          if (e.key === "Enter") {
-            e.preventDefault();
-            startTransition(addTag);
-          }
-        }}
-        value={newTag}
-      />
-
-      <button
-        type="button"
-        onClick={(e) => {
-          e.preventDefault();
-          startTransition(addTag);
-        }}
-      >
-        追加
-      </button>
-      {functionMessage && <p>{functionMessage}</p>}
-    </div>
-  );
-};
