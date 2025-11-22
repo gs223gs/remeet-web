9c5cecc201e410ce4dc29d925ce5e8dc765158be
T.Miura <mtaiga0213@icloud.com>
2025-11-22 15:34:15 +0900

    update: updateForm の分離

--- app/(private)/dashboard/meetup/[meetupId]/contacts/[contactsId]/page.tsx
+++ app/(private)/dashboard/meetup/[meetupId]/contacts/[contactsId]/page.tsx
@@ -1,5 +1,4 @@
 import { getContactDetail } from "../_server/server";
-import { getTags } from "../_server/server";
 
 import { ContactsDetailView } from "@/components/contacts/contactsDetailView";
 
@@ -10,7 +9,6 @@ export default async function ContactsDetail({
 }) {
   const { meetupId, contactsId } = await params;
   const contactsDetailRes = await getContactDetail(contactsId);
-  const tagsRes = await getTags();
 
   if (!contactsDetailRes.ok) {
     return (
@@ -25,15 +23,7 @@ export default async function ContactsDetail({
     );
   }
 
-  if (!tagsRes.ok) return <p>tag取得に失敗しました</p>;
-
   const contactsDetail = contactsDetailRes.data;
 
-  return (
-    <ContactsDetailView
-      meetupId={meetupId}
-      tags={tagsRes.data}
-      contactsDetail={contactsDetail}
-    />
-  );
+  return <ContactsDetailView contactsDetail={contactsDetail} />;
 }
