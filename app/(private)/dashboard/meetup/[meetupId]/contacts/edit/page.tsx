c91f42aa153e5d8a9517629edc3e69ce4cf21525
T.Miura <mtaiga0213@icloud.com>
2025-11-22 15:33:55 +0900

    feat: contacts update page の実装

---
+++ app/(private)/dashboard/meetup/[meetupId]/contacts/edit/page.tsx
@@ -0,0 +1,27 @@
+import { getContactDetail, getTags } from "../_server/server";
+
+import { ContactsUpdateForm } from "@/components/contacts/form/contactsUpdateForm";
+
+export default async function ContactsUpdatePage({
+  params,
+}: {
+  params: Promise<{ meetupId: string; contactsId: string }>;
+}) {
+  const { meetupId, contactsId } = await params;
+  const contactsDetailRes = await getContactDetail(contactsId);
+  const tagsRes = await getTags();
+
+  if (!tagsRes.ok) {
+    return <div>tags error</div>;
+  }
+  if (!contactsDetailRes.ok) {
+    return <div>contacts error</div>;
+  }
+  return (
+    <ContactsUpdateForm
+      meetupId={meetupId}
+      tags={tagsRes.data}
+      contactsDetail={contactsDetailRes.data}
+    />
+  );
+}
