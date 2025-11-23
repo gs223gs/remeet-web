726644f692ad3f955fc89200ad4aed13cb218bf3
T.Miura <mtaiga0213@icloud.com>
2025-11-23 23:25:09 +0900

    feat: useStateAction の実装

--- components/tag/form/updateTag.tsx
+++ components/tag/form/updateTag.tsx
@@ -3,14 +3,25 @@ import { useActionState } from "react";
 
 import type { Tag } from "@/type/private/tags/tags";
 
+import { updateTag } from "@/app/(private)/dashboard/tags/[tagId]/action";
+
 type UpdateTagFormProps = {
   tag: Tag;
 };
 export const UpdateTagForm = ({ tag }: UpdateTagFormProps) => {
-  const [state, action, isPending] = useActionState();
+  const updateTagWithId = updateTag.bind(null, tag.id);
+  const [state, action, isPending] = useActionState(updateTagWithId, {
+    success: false,
+    error: {},
+  });
+
+  if (isPending) {
+    return <div>pending</div>;
+  }
   return (
-    <form action="">
-      <input type="text" defaultValue={tag.name} />
+    <form action={action}>
+      <div>{state && state.success}</div>
+      <input type="text" name="tagName" defaultValue={tag.name} />
       <button type="submit">確定</button>
     </form>
   );
