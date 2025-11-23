957f93e084bea9c4c24d7e426d268de4255a5820
T.Miura <mtaiga0213@icloud.com>
2025-11-24 01:31:42 +0900

    revert: success の type を revet RHFとの相性が悪かった

--- type/util/action.d.ts
+++ type/util/action.d.ts
@@ -1,7 +1,4 @@
-export type ActionState<T> =
-  | {
-      success: boolean;
-      errors: T;
-    }
-  | { success: true }
-  | { success: false; error: T };
+export type ActionState<T> = {
+  success: boolean;
+  errors: T;
+};
