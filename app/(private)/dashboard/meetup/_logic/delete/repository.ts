e4b3e5a7fdc3b77e0ee0bd1a443d696a2967d649
T.Miura <mtaiga0213@icloud.com>
2025-11-25 21:34:38 +0900

    feat:repository の仮実装

---
+++ app/(private)/dashboard/meetup/_logic/delete/repository.ts
@@ -0,0 +1,21 @@
+// "use server";
+// import type { Result } from "@/type/error/error";
+
+// import { prisma } from "@/lib/prisma";
+
+// export const deleteMeetUpRecord = async (
+//   meetupId: string,
+//   userId: string,
+//   //TODO ここリファクタリング対象
+//   //
+// ): Promise<Result<null>> => {
+//   const deleteResult = await prisma.$transaction(async (tx) => {
+//     await tx.conta
+//   });
+
+//   if(!deleteResult.)
+//   return {
+//     ok: true,
+//     data: null,
+//   };
+// };
