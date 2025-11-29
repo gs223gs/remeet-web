ca64c0e2117a3145f76aecd88c950713eea80eb4
T.Miura <mtaiga0213@icloud.com>
2025-11-29 12:46:34 +0900

    update: functionを property access に変更

--- app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/deleteContact.ts
+++ app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/deleteContact.ts
@@ -1,16 +1,30 @@
+import type { RepositoryResult } from "@/type/error/error";
+import type { ContactsErrors } from "@/type/private/contacts/contacts";
+
 import { prisma } from "@/lib/prisma";
 
-export const deleteContactRepository = async (
-  contactId: string,
-  userId: string,
-): Promise<boolean> => {
-  try {
-    await prisma.contact.delete({
-      where: { id: contactId, userId: userId },
-    });
-    return true;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
+export const contactRepository = {
+  async delete(
+    contactId: string,
+    userId: string,
+  ): Promise<RepositoryResult<null, ContactsErrors>> {
+    try {
+      await prisma.contact.delete({
+        where: { id: contactId, userId: userId },
+      });
+
+      return {
+        ok: true,
+        data: null,
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        ok: false,
+        error: {
+          server: "server error",
+        },
+      };
+    }
+  },
 };
