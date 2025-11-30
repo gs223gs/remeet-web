9009484e4445851d49f79bb16cb495b980b28fdf
T.Miura <mtaiga0213@icloud.com>
2025-11-30 18:27:36 +0900

    feat: typedRoute の強化のために routes.ts の追加

---
+++ src/utils/routes.ts
@@ -0,0 +1,23 @@
+export const routes = {
+  home: () => "/" as const,
+  notFound: () => "/_not-found" as const,
+  login: () => "/login" as const,
+  apiAuth: () => "/api/auth/[...nextauth]" as const,
+  dashboard: () => "/dashboard" as const,
+  dashboardContacts: () => "/dashboard/contacts" as const,
+  dashboardMeetupList: () => "/dashboard/meetup" as const,
+  dashboardMeetupNew: () => "/dashboard/meetup/new" as const,
+  dashboardMeetupDetail: (meetupId: string) =>
+    `/dashboard/meetup/${meetupId}` as const,
+  dashboardMeetupEdit: (meetupId: string) =>
+    `/dashboard/meetup/${meetupId}/edit` as const,
+  dashboardMeetupContactNew: (meetupId: string) =>
+    `/dashboard/meetup/${meetupId}/contacts/new` as const,
+  dashboardMeetupContactDetail: (meetupId: string, contactsId: string) =>
+    `/dashboard/meetup/${meetupId}/contacts/${contactsId}` as const,
+  dashboardMeetupContactEdit: (meetupId: string, contactsId: string) =>
+    `/dashboard/meetup/${meetupId}/contacts/${contactsId}/edit` as const,
+  dashboardTags: () => "/dashboard/tags" as const,
+  dashboardTagDetail: (tagId: string) => `/dashboard/tags/${tagId}` as const,
+  dashboardTagEdit: (tagId: string) => `/dashboard/tags/${tagId}/edit` as const,
+} as const;
