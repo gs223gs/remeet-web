b3a5ee054203fd4cce53dc268efa29af759e8dff
T.Miura <mtaiga0213@icloud.com>
2025-12-16 22:26:14 +0900

    meetup/contacts/new form を shadcn に変更

--- components/contacts/form/requiredForm.tsx
+++ components/contacts/form/requiredForm.tsx
@@ -10,12 +10,23 @@ import type { Tag } from "@/type/private/tags/tags";
 
 import { createContacts } from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/action";
 import { NewTag } from "@/components/tag/form/newTag";
+import { Form } from "react-hook-form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 export default function RequiredForm({ meetupId, tags }: Props) {
   const createContactsWithMeetupId = createContacts.bind(null, meetupId);
   const [contactTags, setContactTags] = useState<Tag[]>([...tags]);
   const [selectTags, setSelectTags] = useState<Tag[]>([]);
   const [newTag, setNewTag] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
 
   const [state, action, isPending] = useActionState(
     createContactsWithMeetupId,
@@ -28,108 +39,261 @@ export default function RequiredForm({ meetupId, tags }: Props) {
   if (isPending) return <div>pending</div>;
 
   return (
-    <form action={action} className="flex flex-col m-4 outline">
-      {state.success ? <p>true</p> : <p>false</p>}
-      name:
-      <input type="text" name="name" placeholder="name" />
-      comp: <input type="text" name="company" placeholder="company" />
-      role: <input type="text" name="role" placeholder="role" />
-      disc: <textarea name="description" />
-      <NewTag
-        newTag={newTag}
-        setNewTag={setNewTag}
-        setSelectTags={setSelectTags}
-        selectTags={selectTags}
-      />
-      <div className="outline m-5">
-        {contactTags.map((t) => {
-          return (
-            <div
-              className="outline"
-              key={t.id}
-              onClick={() => {
-                setSelectTags([...selectTags, t]);
-                const filterContactsTags = contactTags.filter(
-                  (c) => t.id != c.id,
-                );
-                setContactTags([...filterContactsTags]);
-              }}
-            >
-              {t.name}
-            </div>
-          );
-        })}
-      </div>
-      <div className="outline m-5">
-        {/*TODO あとでセレクトボックスにする */}
-        {selectTags.map((t) => {
-          return (
-            <div key={t.id} className="outline flex">
-              <input defaultValue={t.id} name="tags" type="hidden" />
-              <span>{t.name}</span>
-              <button
+    <Form {...form}>
+      <form action={action} className="flex flex-col m-4 outline">
+        <p className="">*必須</p>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>名前</FormLabel>
+              <FormControl>
+                <Input placeholder="T.Miura" {...field} />
+              </FormControl>
+              <FormDescription>出会った人の名前を入力</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="company"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>所属企業</FormLabel>
+              <FormControl>
+                <Input placeholder="TMiura.inc" {...field} />
+              </FormControl>
+              <FormDescription>所属企業を入力</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="role"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>役職</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="個人開発者, VPoE, テックリード"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>役職を入力</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>詳細</FormLabel>
+              <FormControl>
+                <Input placeholder="話したことを書きましょう!" {...field} />
+              </FormControl>
+              <FormDescription>マークダウンで書けます</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <NewTag
+          newTag={newTag}
+          setNewTag={setNewTag}
+          setSelectTags={setSelectTags}
+          selectTags={selectTags}
+        />
+        <div className="outline m-5">
+          {contactTags.map((t) => {
+            return (
+              <div
+                className="outline"
+                key={t.id}
                 onClick={() => {
-                  setSelectTags([...selectTags.filter((st) => st != t)]);
+                  if (selectTags.length === 5) {
+                    {
+                      /** error どうしようね */
+                    }
+                    return;
+                  }
+                  setSelectTags([...selectTags, t]);
+                  const filterContactsTags = contactTags.filter(
+                    (c) => t.id != c.id,
+                  );
+                  setContactTags([...filterContactsTags]);
                 }}
-                className=" outline"
-                type="button"
               >
-                x
-              </button>
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        <span>
-          handle:
-          <input type="text" name="githubHandle" />
-        </span>
-        <span>
-          id:
-          <input type="text" name="githubId" />
-        </span>
-      </div>
-      <div>
-        <span>
-          handle:
-          <input type="text" name="twitterHandle" />
-        </span>
-        <span>
-          id:
-          <input type="text" name="twitterId" />
-        </span>
-      </div>
-      <div>
-        <span>
-          handle:
-          <input type="text" name="websiteHandle" />
-        </span>
-        <span>
-          id:
-          <input type="text" name="websiteUrl" />
-        </span>
-      </div>
-      <div>
-        <span>
-          handle:
-          <input type="text" name="productHandle" />
-        </span>
-        <span>
-          id:
-          <input type="text" name="productUrl" />
-        </span>
-      </div>
-      <div>
-        <span>
-          handle:
-          <input type="text" name="otherHandle" />
-          <span>
-            id: <input type="text" name="other" />
-          </span>
-        </span>
-      </div>
-      <button type="submit">送信</button>
-    </form>
+                {t.name}
+              </div>
+            );
+          })}
+        </div>
+        <div className="outline m-5">
+          {/*TODO あとでセレクトボックスにする */}
+          {selectTags.map((t) => {
+            return (
+              <div key={t.id} className="outline flex">
+                <input defaultValue={t.id} name="tags" type="hidden" />
+                <span>{t.name}</span>
+                <button
+                  onClick={() => {
+                    setSelectTags([...selectTags.filter((st) => st != t)]);
+                  }}
+                  className=" outline"
+                  type="button"
+                >
+                  x
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <FormField
+          control={form.control}
+          name="githubHandle"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>GithubHandle</FormLabel>
+              <FormControl>
+                <Input placeholder="githubの名前を入力" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />{" "}
+        <FormField
+          control={form.control}
+          name="githubId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>GithubId</FormLabel>
+              <FormControl>
+                <Input placeholder="GitHubのidを入力" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="twitterHandle"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ハンドルネーム</FormLabel>
+              <FormControl>
+                <Input placeholder="GitHubのidを入力" {...field} />
+              </FormControl>
+              <FormDescription>誰がなと言おうとTwitter</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="githubId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>TwitterId</FormLabel>
+              <FormControl>
+                <Input placeholder="GitHubのidを入力" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="websiteHandle"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>TwitterId</FormLabel>
+              <FormControl>
+                <Input placeholder="プロフィールサイト名" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="websiteUrl"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>URL</FormLabel>
+              <FormControl>
+                <Input placeholder="https://...." {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="productHandle"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>個人開発サービス</FormLabel>
+              <FormControl>
+                <Input placeholder="ReMeet" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="productUrl"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>URL</FormLabel>
+              <FormControl>
+                <Input placeholder="https://...." {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="otherHandle"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>その他</FormLabel>
+              <FormControl>
+                <Input placeholder="https://...." {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="other"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>その他のURL</FormLabel>
+              <FormControl>
+                <Input placeholder="https://...." {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <button type="submit">送信</button>
+      </form>
+    </Form>
   );
 }
