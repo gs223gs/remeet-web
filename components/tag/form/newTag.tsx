24c146aceca37853be9540b1913e295839ede554
T.Miura <mtaiga0213@icloud.com>
2025-12-19 21:57:31 +0900

    fix: 前の状態に戻した リアルタイム検索のため

--- components/tag/form/newTag.tsx
+++ components/tag/form/newTag.tsx
@@ -6,12 +6,18 @@ import type { Tag } from "@/type/private/tags/tags";
 import { createTag } from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/action";
 
 type NewTagProps = {
+  setNewTag: (s: string) => void;
+  newTag: string;
   selectTags: Tag[];
   setSelectTags: (t: Tag[]) => void;
 };
 
-export const NewTag = ({ setSelectTags, selectTags }: NewTagProps) => {
-  const [newTag, setNewTag] = useState<string>("");
+export const NewTag = ({
+  newTag,
+  setNewTag,
+  setSelectTags,
+  selectTags,
+}: NewTagProps) => {
   const [isPending, startTransition] = useTransition();
   const [functionMessage, setFunctionMessage] = useState<string[]>([]);
 
