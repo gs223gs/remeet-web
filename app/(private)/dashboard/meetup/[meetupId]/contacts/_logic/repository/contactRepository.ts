30a6a46ff1b65f25b1bbe3a9638cc17210a758dc
T.Miura <mtaiga0213@icloud.com>
2025-12-23 20:19:59 +0900

    feat: contactRepository に createを実装

--- app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/repository/contactRepository.ts
+++ app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/repository/contactRepository.ts
@@ -1,9 +1,37 @@
-import type { MigrationResult } from "@/type/error/error";
+import type { MigrationResult, Result } from "@/type/error/error";
 import type { ContactsErrors } from "@/type/private/contacts/contacts";
 
 import { prisma } from "@/lib/prisma";
-
+type AddContacts = {
+  meetupId: string;
+  userId: string;
+  name: string;
+  company: string | undefined;
+  role: string | undefined;
+  description: string | undefined;
+};
 export const contactRepository = {
+  async create(data: AddContacts): Promise<Result<string>> {
+    try {
+      const createdContact = await prisma.contact.create({
+        data,
+      });
+
+      return {
+        ok: true,
+        data: createdContact.id,
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        ok: false,
+        error: {
+          code: "db_error",
+          message: ["dbエラーです"],
+        },
+      };
+    }
+  },
   async delete(
     contactId: string,
     userId: string,
