e888ede0a412407e2b6bf9f4cf983adef0524fcb
T.Miura <mtaiga0213@icloud.com>
2025-12-25 23:05:04 +0900

    update: form名が逆だった　なぜ

--- app/(private)/dashboard/meetup/[meetupId]/contacts/new/page.tsx
+++ app/(private)/dashboard/meetup/[meetupId]/contacts/new/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { getTags } from "../_server/server";
 
 import { ContactsErrorCard } from "@/components/contacts/contacts-error-card";
-import { CreateContactForm } from "@/components/contacts/form/CreateContactForm";
+import { ContactForm } from "@/components/contacts/form/ContactForm";
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
 import { Button } from "@/components/ui/button";
 import { routes } from "@/util/routes";
@@ -50,7 +50,7 @@ export default async function CreateContacts({
           </Button>
         </div>
       </div>
-      <CreateContactForm meetupId={meetupId} tags={tags.data} />
+      <ContactForm meetupId={meetupId} tags={tags.data} />
     </div>
   );
 }
