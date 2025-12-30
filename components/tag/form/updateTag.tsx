a816fca90f0bdf07cfa6655302a33824385c790a
T.Miura <mtaiga0213@icloud.com>
2025-12-31 00:54:58 +0900

    update: tag update form の正式実装

--- components/tag/form/updateTag.tsx
+++ components/tag/form/updateTag.tsx
@@ -1,9 +1,25 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useActionState } from "react";
+import { useForm } from "react-hook-form";
 
 import type { Tag } from "@/type/private/tags/tags";
 
 import { updateTag } from "@/app/(private)/dashboard/tags/[tagId]/action";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  tagSchema,
+  type TagSchema,
+} from "@/validations/private/tagValidations";
 
 type UpdateTagFormProps = {
   tag: Tag;
@@ -15,17 +31,42 @@ export const UpdateTagForm = ({ tag }: UpdateTagFormProps) => {
     errors: {},
   });
 
+  const form = useForm<TagSchema>({
+    resolver: zodResolver(tagSchema),
+    defaultValues: {
+      name: tag.name,
+    },
+    mode: "onChange",
+  });
+  const isDisabled = !form.formState.isValid || isPending;
+  const buttonLabel = isPending
+    ? "送信中"
+    : form.formState.isValid
+      ? "送信"
+      : "入力してください";
   if (isPending) {
     return <div>pending</div>;
   }
   return (
-    <form action={action}>
-      {state.errors.id}
-      {state.errors.auth}
-      {state.errors.server}
-      <input type="text" name="name" defaultValue={tag.name} />
-      {state.errors.tag}
-      <button type="submit">確定</button>
-    </form>
+    <Form {...form}>
+      <form action={action}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>tag名</FormLabel>
+              <FormControl>
+                <Input {...field} defaultValue={form.getValues("name")} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={isDisabled}>
+          {buttonLabel}
+        </Button>
+      </form>
+    </Form>
   );
 };
