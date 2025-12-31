b6068fbe282ff868f1d9bf8f00e1550fc1bb0807
T.Miura <mtaiga0213@icloud.com>
2026-01-01 03:44:49 +0900

    feat: contacts ページの loading を実装

---
+++ app/(private)/dashboard/contacts/loading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ContactCardGridSkeleton } from "@/components/skeletons/ContactCardGridSkeleton";
+import { DashboardPageSkeleton } from "@/components/skeletons/DashboardPageSkeleton";
+
+export default function Loading() {
+  return (
+    <DashboardPageSkeleton>
+      <section className="space-y-4">
+        <ContactCardGridSkeleton />
+      </section>
+    </DashboardPageSkeleton>
+  );
+}
