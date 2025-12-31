8a4fe6371b5a6c0ee604ee7e7df106e06a83f04d
T.Miura <mtaiga0213@icloud.com>
2026-01-01 04:11:49 +0900

    feat: dashboard page の loading.tsxを実装

---
+++ app/(private)/dashboard/loading.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { DashboardPageSkeleton } from "@/components/skeletons/DashboardPageSkeleton";
+import { DashboardStatsSkeleton } from "@/components/skeletons/DashboardStatsSkeleton";
+import { LatestMeetupCardSkeleton } from "@/components/skeletons/LatestMeetupCardSkeleton";
+
+export default function Loading() {
+  return (
+    <DashboardPageSkeleton>
+      <section className="flex flex-col gap-6">
+        <DashboardStatsSkeleton />
+        <LatestMeetupCardSkeleton />
+      </section>
+    </DashboardPageSkeleton>
+  );
+}
