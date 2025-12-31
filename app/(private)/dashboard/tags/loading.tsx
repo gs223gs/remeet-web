37b06e999e13b970bf4f31fcb2d238bbe642a279
T.Miura <mtaiga0213@icloud.com>
2026-01-01 04:09:54 +0900

    feat: tag page の loading.tsx を実装

---
+++ app/(private)/dashboard/tags/loading.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { DashboardPageSkeleton } from "@/components/skeletons/DashboardPageSkeleton";
+import { TagsGridSkeleton } from "@/components/skeletons/TagsGridSkeleton";
+import { TagsOverviewSkeleton } from "@/components/skeletons/TagsOverviewSkeleton";
+
+export default function Loading() {
+  return (
+    <DashboardPageSkeleton>
+      <section className="flex flex-col gap-6">
+        <TagsOverviewSkeleton />
+        <TagsGridSkeleton />
+      </section>
+    </DashboardPageSkeleton>
+  );
+}
