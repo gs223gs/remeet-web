c4c560b1f3880e81d3e8ac28caf10fa964f018d3
T.Miura <mtaiga0213@icloud.com>
2026-01-02 03:20:40 +0900

    update: theme toggle を変更 modalに入れるため

--- components/ui/color-mode-toggle.tsx
+++ components/ui/color-mode-toggle.tsx
@@ -1,40 +1,34 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { Check } from "lucide-react";
 import { useTheme } from "next-themes";
 import * as React from "react";
 
-import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
   DropdownMenuItem,
-  DropdownMenuTrigger,
+  DropdownMenuSubTrigger,
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+    <DropdownMenuSub>
+      <DropdownMenuSubTrigger>
+        <span>Toggle theme</span>
+      </DropdownMenuSubTrigger>
+      <DropdownMenuSubContent>
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          {theme === "light" && <Check />}
+          ライトモード
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          {theme === "dark" && <Check />}
+          ダークモード
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </DropdownMenuSubContent>
+    </DropdownMenuSub>
   );
 }
