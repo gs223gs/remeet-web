d9590f2dc76b4add1ea3ee2bf683928f56d87ee1
T.Miura <mtaiga0213@icloud.com>
2026-01-02 20:06:17 +0900

    Update auth.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- auth.ts
+++ auth.ts
@@ -3,8 +3,6 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-import type { Result } from "@/type/error/error";
-
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
