4c645f90def007f951240fe799ae6dd3f9e36cae
T.Miura <mtaiga0213@icloud.com>
2026-01-04 22:08:32 +0900

    feat: header を適応

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -9,26 +9,6 @@ import { routes } from "@/util/routes";
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      <header className="border-b border-gray-200">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex flex-row gap-4 items-center ">
-              <RemeetIcon size={50} />
-              <RemeetFullLogo className="text-orange-500" fill="#fff" />
-            </div>
-
-            <nav className="flex items-center space-x-8">
-              <Button
-                className="bg-orange-500 text-white hover:bg-orange-500/90"
-                asChild
-              >
-                <Link href={routes.login()}>ログイン</Link>
-              </Button>
-            </nav>
-          </div>
-        </div>
-      </header>
-
       <main>
         <section className="pt-20 pb-32 px-4 sm:px-6 lg:px-8">
           <div className="max-w-7xl mx-auto text-center">
