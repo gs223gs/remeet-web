ce234f6451e97ec0e69eef0e9edd24fc59f1dce0
T.Miura <mtaiga0213@icloud.com>
2026-01-17 12:21:57 +0900

    update: 過剰に複雑になっていたので削除

--- app/(private)/dashboard/meetup/createMeetupService.ts
+++ app/(private)/dashboard/meetup/createMeetupService.ts
@@ -1,42 +1,19 @@
 import type { Result } from "@/type/error/error";
 
 import { meetupRepository } from "@/app/(private)/dashboard/meetup/_logic/repository/meetupRepository";
-import { getUser } from "@/auth";
-import { createMeetupSchema } from "@/validations/private/meetupValidation";
 
 export const createMeetupService = async (
-  formData: FormData,
+  userId: string,
+  validatedFields: {
+    meetupName: string;
+    scheduledAt: Date;
+  },
 ): Promise<Result<{ meetupId: string }>> => {
   try {
-    const rawFormData = {
-      name: formData.get("name")?.toString() ?? "",
-      scheduledAt: formData.get("scheduledAt")?.toString() ?? "",
-    };
-
-    const validatedFields = createMeetupSchema.safeParse(rawFormData);
-    if (!validatedFields.success)
-      return {
-        ok: false,
-        error: {
-          code: "validation",
-          message: [],
-        },
-      };
-
-    const user = await getUser();
-    if (!user)
-      return {
-        ok: false,
-        error: {
-          code: "validation",
-          message: [],
-        },
-      };
-
     const createMeetupData = {
-      userId: user.id,
-      name: validatedFields.data.name,
-      scheduledAt: validatedFields.data.scheduledAt,
+      userId: userId,
+      name: validatedFields.meetupName,
+      scheduledAt: validatedFields.scheduledAt,
     };
 
     const createdMeetupResult = await meetupRepository.create(createMeetupData);
