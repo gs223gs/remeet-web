eec161b882bb98f8b5ebc1ecbf30af6ec167fd2d
T.Miura <mtaiga0213@icloud.com>
2026-01-17 12:33:53 +0900

    feat: delete meetup service の実装

---
+++ app/(private)/dashboard/meetup/deleteMeetupService.ts
@@ -0,0 +1,47 @@
+import type { Result } from "@/type/error/error";
+
+import { meetupRepository } from "@/app/(private)/dashboard/meetup/_logic/repository/meetupRepository";
+
+export const deleteMeetupService = async (
+  userId: string,
+  meetupId: string,
+): Promise<Result<void>> => {
+  try {
+    const verifyUserOwnedMeetup = await meetupRepository.verifyUserOwnedMeetup(
+      userId,
+      meetupId,
+    );
+    if (!verifyUserOwnedMeetup.ok)
+      return {
+        ok: false,
+        error: {
+          code: "authorization",
+          message: [],
+        },
+      };
+
+    const deleteMeetupResult = await meetupRepository.delete(meetupId, userId);
+    if (!deleteMeetupResult.ok)
+      return {
+        ok: false,
+        error: {
+          code: "db_error",
+          message: [],
+        },
+      };
+
+    return {
+      ok: true,
+      data: undefined,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      ok: false,
+      error: {
+        code: "unknown",
+        message: [],
+      },
+    };
+  }
+};
