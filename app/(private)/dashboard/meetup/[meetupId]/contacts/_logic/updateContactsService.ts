f302914aaa1026165980bb2e55d43b4211a7b746
T.Miura <mtaiga0213@icloud.com>
2026-01-18 21:41:34 +0900

    feat: update service を実装

---
+++ app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/updateContactsService.ts
@@ -0,0 +1,99 @@
+import { convertInsertableLinks } from "./convertInsertableLinks";
+
+import type { ContactsFormData } from "@/type/private/contacts/contacts";
+
+import { linkRepository } from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/linkRepository";
+import { contactRepository } from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/repository/contactRepository";
+import { meetupRepository } from "@/app/(private)/dashboard/meetup/_logic/repository/meetupRepository";
+import { tagRepository } from "@/app/(private)/dashboard/tags/_server/tagRepository";
+import { prisma } from "@/lib/prisma";
+
+export const updateContactsService = async (
+  meetupId: string,
+  contactId: string,
+  userId: string,
+  validatedFields: ContactsFormData,
+) => {
+  const verifyOwnedMeetup = await meetupRepository.verifyUserOwnedMeetup(
+    userId,
+    meetupId,
+  );
+
+  //TODO ここあとで整える
+  if (!verifyOwnedMeetup.ok)
+    return {
+      ok: false,
+      error: {
+        code: "authorization",
+      },
+    };
+
+  const validatedTagId = validatedFields.tags;
+  if (validatedTagId?.length) {
+    const verifiedTag = await tagRepository.validateOwnedTagsExistence(
+      userId,
+      validatedTagId,
+    );
+    if (!verifiedTag.ok) {
+      return {
+        ok: false,
+        error: {
+          code: "authorization",
+        },
+      };
+    }
+  }
+
+  const insertableLinks = convertInsertableLinks(validatedFields);
+
+  const updateContactsData = {
+    contactId,
+    meetupId,
+    userId,
+    name: validatedFields.name,
+    company: validatedFields.company,
+    role: validatedFields.role,
+    description: validatedFields.description,
+  };
+
+  await prisma.$transaction(async (tx) => {
+    const createdContact = await contactRepository.update(
+      tx,
+      updateContactsData,
+    );
+    if (!createdContact.ok) {
+      throw new Error("abort transaction");
+    }
+
+    //ここrepository にした方がいいかも
+    await tx.contactLink.deleteMany({
+      where: { contactId: contactId },
+    });
+
+    if (insertableLinks.length) {
+      const createdLinks = await linkRepository.create(
+        tx,
+        createdContact.data,
+        insertableLinks,
+      );
+
+      if (!createdLinks.ok) throw new Error("abort transaction");
+    }
+
+    //ここrepository にした方がいいかも
+    await tx.contactTag.deleteMany({
+      where: { contactId: contactId },
+    });
+
+    if (validatedTagId?.length) {
+      const createdContactTags = await tagRepository.createContactTag(
+        tx,
+        createdContact.data,
+        validatedTagId,
+      );
+      if (!createdContactTags.ok) {
+        throw new Error("abort transaction");
+      }
+    }
+  });
+};
