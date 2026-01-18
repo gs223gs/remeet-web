dfdbe8238f2f9d51b6187ef180f3c0d83e2b728f
T.Miura <mtaiga0213@icloud.com>
2026-01-18 22:01:22 +0900

    refactor: service の適応

--- app/(private)/dashboard/meetup/[meetupId]/contacts/action.ts
+++ app/(private)/dashboard/meetup/[meetupId]/contacts/action.ts
@@ -2,15 +2,13 @@
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { redirect } from "next/navigation";
 
+import { updateContactsService } from "./_logic/updateContactsService";
+
 import type { ErrorCode } from "@/type/error/error";
 import type { Result } from "@/type/error/error";
-import type {
-  ContactsErrors,
-  CreateContactLink,
-} from "@/type/private/contacts/contacts";
+import type { ContactsErrors } from "@/type/private/contacts/contacts";
 import type { Tag } from "@/type/private/tags/tags";
 import type { ActionState } from "@/type/util/action";
-import type { LinkType } from "@prisma/client";
 
 import { contactValidation } from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/contactsValidation";
 import { createContactService } from "@/app/(private)/dashboard/meetup/[meetupId]/contacts/_logic/createContactsService";
@@ -58,33 +56,6 @@ export const createContacts = async (
   }
 };
 
-/**
- *
- * @param tags
- * @returns boolean
- *
- * @description フロントから送られてきたタグをDBで検証する関数 フロントから送られてきたタグの数とDB fetchしたタグの数が一致していれば true を返す
- */
-const verifyTags = async (tags: string[], userId: string): Promise<boolean> => {
-  try {
-    const fetchTags = await prisma.tag.findMany({
-      where: {
-        userId: userId,
-        id: {
-          in: tags,
-        },
-      },
-    });
-
-    if (fetchTags.length !== tags.length) return false;
-
-    return true;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-};
-
 //TODO v1.2.1 で refactoring 対象
 export const updateContacts = async (
   meetupId: string,
@@ -92,17 +63,7 @@ export const updateContacts = async (
   _: ActionState<ContactsErrors> | null,
   formData: FormData,
 ): Promise<ActionState<ContactsErrors>> => {
-  const user = await getUser();
-  if (!user)
-    return {
-      success: false,
-      errors: {
-        auth: "認証に失敗しました",
-      },
-    };
-
   const validatedFields = contactValidation(formData);
-
   if (!validatedFields.success)
     //TODO return の値を変更しろ
     return {
@@ -112,103 +73,28 @@ export const updateContacts = async (
       },
     };
 
-  try {
-    const tags = validatedFields.data.tags;
-    if (tags?.length) {
-      const isVerifyTags = await verifyTags(tags, user.id);
-      if (!isVerifyTags) {
-        return {
-          success: false,
-          errors: { auth: "認証に失敗しました" },
-        };
-      }
-    }
+  const user = await getUser();
+  if (!user)
+    return {
+      success: false,
+      errors: {
+        auth: "認証に失敗しました",
+      },
+    };
 
-    await prisma.$transaction(async (tx) => {
-      await tx.contact.update({
-        where: { id: contactId, userId: user.id },
-        data: {
-          userId: user.id,
-          name: validatedFields.data.name,
-          company: validatedFields.data.company,
-          role: validatedFields.data.role,
-          description: validatedFields.data.description,
-        },
-      });
-
-      await tx.contactLink.deleteMany({
-        where: { contactId: contactId },
-      });
-
-      const insertLinks = [
-        {
-          type: "GITHUB" as LinkType,
-          url: validatedFields.data.githubId,
-          handle: validatedFields.data.githubHandle,
-        },
-        {
-          type: "TWITTER" as LinkType,
-          url: validatedFields.data.twitterId,
-          handle: validatedFields.data.twitterHandle,
-        },
-        {
-          type: "WEBSITE" as LinkType,
-          url: validatedFields.data.websiteUrl,
-          handle: validatedFields.data.websiteHandle,
-        },
-        {
-          type: "OTHER" as LinkType,
-          url: validatedFields.data.other,
-          handle: validatedFields.data.otherHandle,
-        },
-        {
-          type: "PRODUCT" as LinkType,
-          url: validatedFields.data.productUrl,
-          handle: validatedFields.data.productHandle,
-        },
-      ] as const;
-
-      const filterInsertLinks: CreateContactLink[] = insertLinks.flatMap((l) =>
-        l.url
-          ? [
-              {
-                contactId: contactId,
-                type: l.type,
-                url: l.url,
-                ...(l.handle ? { handle: l.handle } : {}),
-              },
-            ]
-          : [],
-      );
-
-      if (filterInsertLinks.length) {
-        await tx.contactLink.createMany({ data: filterInsertLinks });
-      }
-
-      await tx.contactTag.deleteMany({
-        where: { contactId: contactId },
-      });
-
-      //TODO not elegantですね
-      if (tags?.length) {
-        const insertTags = tags.map((t) => {
-          return { contactId: contactId, tagId: t };
-        });
-        await tx.contactTag.createMany({
-          data: insertTags,
-        });
-      }
-    });
-
-    redirect(`/dashboard/meetup/${meetupId}/contacts/${contactId}`);
-  } catch (error) {
-    if (isRedirectError(error)) throw error;
-    console.error(error);
+  const updateServiceResult = await updateContactsService(
+    meetupId,
+    contactId,
+    user.id,
+    validatedFields.data,
+  );
+  if (!updateServiceResult)
     return {
       success: false,
       errors: {},
     };
-  }
+
+  redirect(`/dashboard/meetup/${meetupId}/contacts/${contactId}`);
 };
 //TODO validation
 export const createTag = async (newTag: string): Promise<Result<Tag>> => {
